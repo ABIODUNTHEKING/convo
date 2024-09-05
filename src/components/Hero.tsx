940a75f3a4e6543fe9d0c55b181cc953df3540f2
ABIODUNTHEKING <abioduntheking@gmail.com>
2024-09-05 04:04:24 +0100

    Remove pink background

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ import Button from './Button'
 function Hero() {
   return (
     <section className='md:w-1/2 mx-auto text-gray-2 text-center'>
-        <Image src={image} alt='Convo logo' className='bg-pink-700 w-[38rem]  mx-auto' />
+        <Image src={image} alt='Convo logo' className='w-[38rem]  mx-auto' />
         <div className='md:-mt-28'>
         <p className='bg-bold w-3/5 mx-auto mb-5 md:mb-10 md:text-2xl'>The Future of Community Management</p>
         <Button text='Get started' href='' bgColor='black' />
